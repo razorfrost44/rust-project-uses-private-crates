d9324194455d83044db48ef82c2e9b998de67556
Raymond Gottsch <rgottsch@logitech.com>
2025-04-23 12:25:40 -0700

    Add usage of homemade private crate hosted on github

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use simple_add_crate::simple_add::add_two;
+
+fn run_simple_add_crate_example() {
+    println!("Attempting to use the homemade simple_add_crate from it's own repository.");
+    let a = 5;
+    let b = 10;
+    let result = add_two(a, b);
+    println!("The result of adding {} and {} is: {}", a, b, result);
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("START");
+    println!();
+
+    run_simple_add_crate_example();
+
+    println!();
+    println!("END");
 }
